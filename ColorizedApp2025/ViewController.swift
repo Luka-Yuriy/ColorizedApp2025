5971fc482eca2617a228c908c0eb975ae951fd71
Yuri Luka <lukayurii.work@gmail.com>
2024-12-03 13:55:54 +0500

    Work with stack views

--- ColorizedApp2025/ViewController.swift
+++ ColorizedApp2025/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
